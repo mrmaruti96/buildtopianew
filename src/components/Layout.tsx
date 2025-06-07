bb5ca1a57516dba017ad5e870862b341d573d3a8
Maruti <112685664+mrmaruti96@users.noreply.github.com>
2025-06-07 19:11:12 +0530

    Update Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -147,7 +147,7 @@ export default function Layout() {
                     className="font-minecraft w-full bg-gray-800/70 px-2 py-1 cursor-pointer border-2 border-gray-900 rounded-lg"
                     onClick={() => handleCopyIP('mc.buildtopiasmp.fun')}
                   >
-                    play.buildtopiasmp.fun
+                    mc.buildtopiasmp.fun
                     {copied && <Check className="inline ml-2 text-green-500 h-4 w-4" />}
                   </div>
                 </div>
@@ -160,9 +160,9 @@ export default function Layout() {
                   <div className="text-sm text-gray-300 pixel-text">IP:</div>
                   <div 
                     className="col-span-2 font-minecraft bg-gray-800/70 px-2 py-1 border-2 border-gray-900 cursor-pointer flex items-center justify-between rounded-lg"
-                    onClick={() => handleCopyIP('play.buildtopiasmp.fun')}
+                    onClick={() => handleCopyIP('mcpe.buildtopiasmp.fun')}
                   >
-                    <span>mc.buildtopiasmp.fun</span>
+                    <span>mcpe.buildtopiasmp.fun</span>
                     {copied && <Check className="inline ml-2 text-green-500 h-4 w-4" />}
                   </div>
                   <div className="text-sm text-gray-300 pixel-text">Port:</div>
@@ -170,7 +170,7 @@ export default function Layout() {
                     className="col-span-2 font-minecraft bg-gray-800/70 px-2 py-1 border-2 border-gray-900 cursor-pointer flex items-center justify-between"
                     onClick={() => handleCopyIP('25569')}
                   >
-                    <span>6004</span>
+                    <span>25569</span>
                     {copied && <Check className="inline ml-2 text-green-500 h-4 w-4" />}
                   </div>
                 </div>
