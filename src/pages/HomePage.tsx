1677fbec7ed54ce869aa3afe9e0083653f5f42e7
Maruti <112685664+mrmaruti96@users.noreply.github.com>
2025-10-13 07:26:55 +0530

    Update HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -165,7 +165,7 @@ export default function HomePage() {
                       IP: <span className="text-green-400">play.buildtopiasmp.fun</span>
                     </div>
                     <div className="bg-black/70 px-3 py-1 rounded">
-                      Port: <span className="text-yellow-400">25597</span>
+                      Port: <span className="text-yellow-400">19140</span>
                     </div>
                   </div>
                 </div>
